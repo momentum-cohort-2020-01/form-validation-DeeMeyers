0285feddebd9b0b8a57740a91f2c1237f8a4b88c
DeeMeyers <justdmeyers@gmail.com>
2020-01-31 17:18:55 -0500

    step 1

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-console.log('Add validation!');
+
+//add a class to all the input boxes in html
+//make querry all for all boxes
+//make a for let of item of boxes that will then do the actual check of 
+    //of wether or not the feild is filled
+//use css and showstring to highlight or not highlight boxes
+
+//make the loop triggered by the submit button
+    //add a click eventListener for the submit button
 
 
-let button = document.getElementById('submit-button')
+let blanks = document.querySelectorAll("input");
+console.log(blanks);
+let form = document.querySelector('#parking-form');
 
-button.addEventListener("click", function(event){
-    
-
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    for(let item of blanks){
+        console.log(item.value);
+        console.log(item.innerText);
+        if( item.value == ''){
+            console.log("invalid");
+            item.parentElement.classList.add("input-invalid");
+        }
+        else{
+            console.log("valid");
+            item.parentElement.classList.add("input-valid");
+        }
+    }
 })
+
