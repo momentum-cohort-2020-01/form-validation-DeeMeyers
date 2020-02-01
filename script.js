2fbf702f4bde514e42e4fc32822ecae24e2bc9fa
DeeMeyers <justdmeyers@gmail.com>
2020-02-01 15:18:00 -0500

    adds message to invalid but keeps adding it

--- script.js
+++ script.js
@@ -9,23 +9,67 @@
     //add a click eventListener for the submit button
 
 
+//Step One -Complete
+
+// let blanks = document.querySelectorAll("input");
+// console.log(blanks);
+// let form = document.querySelector('#parking-form');
+
+// form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     for(let item of blanks){
+//         console.log(item.value);
+//         console.log(item.innerText);
+//         if( item.value == ''){
+//             console.log("invalid");
+//             item.parentElement.classList.add("input-invalid");
+//         }
+//         else{
+//             console.log("valid");
+//             item.parentElement.classList.add("input-valid");
+
+
+//         }
+//     }
+// })
+
+
+//Step Two -working on it
+
 let blanks = document.querySelectorAll("input");
 console.log(blanks);
 let form = document.querySelector('#parking-form');
 
 form.addEventListener("submit", function(event){
-    event.preventDefault();
+    event.preventDefault();//ask what this is doing
     for(let item of blanks){
-        console.log(item.value);
-        console.log(item.innerText);
+        
         if( item.value == ''){
             console.log("invalid");
             item.parentElement.classList.add("input-invalid");
+            let parentEl = item.parentNode;
+            let errorMessage = document.createElement('p');
+            errorMessage.classList.add("input-hint");
+            let errorText = document.createTextNode("*attention");
+            errorMessage.appendChild(errorText); 
+            item.parentElement.appendChild(errorMessage);
+
+            // item.appendChild(errorMessage);
+
+
+            
+            
+        
+
         }
         else{
             console.log("valid");
             item.parentElement.classList.add("input-valid");
+            
+
+
         }
     }
 })
+
 
